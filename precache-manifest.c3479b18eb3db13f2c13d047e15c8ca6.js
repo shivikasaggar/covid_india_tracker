60cfdc3e834be3cdf24db1225745ef26f7e88d5b
shivikasaggar <shivikasagar@gmail.com>
2020-06-13 10:22:57 +0530

    Updates

---
+++ precache-manifest.c3479b18eb3db13f2c13d047e15c8ca6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac658e30f59abccb9f30461d10dd3bf",
+    "url": "/shivikasaggar/covid_india_tracker.git/index.html"
+  },
+  {
+    "revision": "eb8d6aaac53cc596101d",
+    "url": "/shivikasaggar/covid_india_tracker.git/static/css/main.2dd3a3bc.chunk.css"
+  },
+  {
+    "revision": "f63b9642c0feeef77fc1",
+    "url": "/shivikasaggar/covid_india_tracker.git/static/js/2.4a447fb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shivikasaggar/covid_india_tracker.git/static/js/2.4a447fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb8d6aaac53cc596101d",
+    "url": "/shivikasaggar/covid_india_tracker.git/static/js/main.3ea10e03.chunk.js"
+  },
+  {
+    "revision": "d114eaa97353b70c9206",
+    "url": "/shivikasaggar/covid_india_tracker.git/static/js/runtime-main.51269351.js"
+  },
+  {
+    "revision": "05d52988a72cb1da374bc1690f060224",
+    "url": "/shivikasaggar/covid_india_tracker.git/static/media/1.05d52988.png"
+  },
+  {
+    "revision": "6cd18bc0983f441a1b55d38408a271a7",
+    "url": "/shivikasaggar/covid_india_tracker.git/static/media/2.6cd18bc0.png"
+  },
+  {
+    "revision": "ba75e0c9c558e20911df32aa3cc7a5bd",
+    "url": "/shivikasaggar/covid_india_tracker.git/static/media/3.ba75e0c9.png"
+  }
+]);
